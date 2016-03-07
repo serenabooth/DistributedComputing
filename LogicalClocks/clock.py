c147f2f0d8bd36fa2f3c44bad0b51e663adbe672
Serena Booth <sbooth@college.harvard.edu>
2016-03-07 18:54:52 -0500

    sockets communicating

--- LogicalClocks/clock.py
+++ LogicalClocks/clock.py
@@ -6,67 +6,94 @@ from threading import *
 import Queue
 import random
 
+# cite: http://stackoverflow.com/questions/19846332/python-threading-inside-a-class
+def threaded(fn):
+    def wrapper(*args, **kwargs):
+        Thread(target=fn, args=args, kwargs=kwargs).start()
+    return wrapper
+
 class Clock(Thread):
     # shared between all processes / logical clocks
     global socket_connections
     socket_connections = {}
 
-    def __init__(self, id, ticks_per_min, logbook, port):
+    def __init__(self, id, ticks_per_min, logbook, port_client, port_server):
         Thread.__init__(self)
         print "Clock " + str(id) + " started"
 
         self.id = id
         self.ticks_per_min = ticks_per_min
         self.logbook = logbook
-        self.clock_time = 0
 
+        self.clock_time = 0
         self.msg_queue = Queue.Queue()
 
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.s.settimeout(self.ticks_per_min)
-        try:
-            self.s.bind(('', port))
-        except socket.error as msg:
-            print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message : ' + str(msg[1])
-            sys.exit()
-        self.s.listen(10)
+        # keep track of the ports
+        self.port_client = port_client
+        self.port_server = port_server
+
+        # set up client (receiving) socket
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client.settimeout(None)
+
 
         global socket_connections
-        socket_connections[str(self.id)] = self.s 
+        socket_connections[str(self.id)] = self.client
+
+    # @threaded 
+    # def server_receive(self):
+    #     print "TO DO"
+
+
+    @threaded 
+    def client_do_stuff(self):
+        operation_instr = 1 #random.randint(1,3)
+        if operation_instr == 1: 
+            self.send_event(socket_connections['1'])
+        elif operation_instr == 2: 
+            self.receive_event()
+        elif operation_instr == 3: 
+            self.internal_event()
 
     def run(self):
         global socket_connections
+
         while True: 
             print str(datetime.now()) + ": Clock " + str(self.id) + " says hello"
 
-            #time.sleep(5)
-            tmp_timeout = self.s.gettimeout()
+            print "connecting to server"
+
+            # set up server (listening) socket
+            self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.server.settimeout(self.ticks_per_min)
+            self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            try:
+                self.server.bind(('', self.port_server))
+            except socket.error as msg:
+                print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message : ' + str(msg[1])
+                sys.exit()
+            self.server.listen(10)
+
+            try: 
+                print "accepting messages on server"
+                c, addr = self.server.accept()
+                data, addr_2 = c.recvfrom(1024)
+                print "trying to get data "
+                #data = self.server.recvfrom(1024)
+                if data: 
+                    self.msg_queue.append(data)
+                    print "got some! " + data
+                    # does this work? 
+                c.close()
+
+            except Exception, e: 
+                self.server.close()
+                print "exception" + str(e)
+                print "complete an instruction"
+                self.client_do_stuff()
 
 
-            while (tmp_timeout > 0): 
-                try: 
-                    tmp_timeout = self.s.gettimeout()
-                    # If the socket hasn't timed out, receive incoming messages.
-                    c, addr = self.s.accept()
-                    print "trying to get data "
-                    data = c.recv(1024).decode()
-                    if data: 
-                        self.msg_queue.append(data)
-                        print "got some! " + data
-                        self.s.settimeout(tmp_timeout)
-                    c.close()
-                except socket.timeout: 
-                    tmp_timeout = 0
-                    self.s.settimeout(self.ticks_per_min)
-                    # If the socket has timed out, restart the timeout, and perform an instruction.
-                    print "complete an instruction"
-                    operation_instr = 1
-                    if operation_instr == 1: 
-                        self.send_event(socket_connections['1'])
-                    elif operation_instr == 2: 
-                        self.receive_event()
-                    elif operation_instr == 3: 
-                        self.internal_event()
+            # If the socket has timed out, restart the timeout, and perform an instruction.
 
     def log(self, msg=None):
         f = open(self.logbook, 'a')
@@ -81,19 +108,19 @@ class Clock(Thread):
     def send_event(self, dst):
         #self.s.settimeout(10)
         print "trying to send"
-        try: 
-            c, addr = dst.accept()
-            print "c " + c
-            print "addr " + addr
-            msg = 'Hello other logical clock'
-            sent = addr.send(msg.encode())
-            if sent == 0:
-                raise RuntimeError("socket connection broken")
-            c.close()
-        except socket.timeout: 
-            print "During send, socket timeout"
-        self.clock_time += 1
-        print "SEND - TO DO"
+        # try: 
+        #     c, addr = dst.accept()
+        #     print "c " + c
+        #     print "addr " + addr
+        #     msg = 'Hello other logical clock'
+        #     sent = c.send(msg)
+        #     if sent == 0:
+        #         raise RuntimeError("socket connection broken")
+        #     c.close()
+        # except socket.timeout: 
+        #     print "During send, socket timeout"
+        # self.clock_time += 1
+        # print "SEND - TO DO"
 
     def internal_event (self):
         self.clock_time += 1
