557ffa9b01a0b765bee7984fbf8a9ea45f46cc7e
Michelle Cone <mcone@college.harvard.edu>
2016-04-25 20:06:56 -0400

    First leader election working

---
+++ ConnectedHearts/main.py
@@ -0,0 +1,18 @@
+from hearts import *
+
+bulb_objects_dict = {}
+
+for i in range(1, 13):
+	bulb_objects_dict["bulb_" + str(i)] = Bulb(i)
+	bulb_objects_dict["bulb_" + str(i)].id = i
+
+bulb_objects_list = bulb_objects_dict.values()
+
+for bulb in bulb_objects_list:
+	bulb.register_bulbs(bulb_objects_list)
+
+for bulb in bulb_objects_list:
+	bulb.start()
+
+for bulb in bulb_objects_list:
+	bulb.first_leader_election()
