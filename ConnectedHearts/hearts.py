8b6bbb55f61a72e2fada0edbe1a16cd1e03aa1a2
Michelle Cone <mcone@college.harvard.edu>
2016-04-26 18:40:08 -0400

    Timeout if can't connect to leader

--- ConnectedHearts/hearts.py
+++ ConnectedHearts/hearts.py
@@ -60,9 +60,10 @@ class Bulb(Thread):
             self.set_up_leader_socket()
         else:
             print "Hi, I'm a follower: " + str(self.id) + "\n"
-            connection_timeout = random.random()
+            connection_timeout = random.randint(1,20)
             self.connect_to_leader_socket(connection_timeout, time.time())
 
+    @threaded
     def set_up_leader_socket(self):
         try: 
             self.leader_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -70,21 +71,24 @@ class Bulb(Thread):
             try:
                 self.leader_socket.bind(('', self.leader_socket_port))
             except socket.error as msg:
-                print "Bind failed. Error Code : " \
-                        + str(msg[0]) + " Message : " + str(msg[1]) + "\n"
+                sys.stderr.write("Bind failed. Error Code : " \
+                        + str(msg[0]) + " Message : " + str(msg[1]) + "\n")
                 sys.exit()
             self.leader_socket.listen(12)
-            self.server.shutdown(socket.SHUT_RDWR)
-            self.server.close()
+            """try:
+                self.leader_socket.shutdown(socket.SHUT_RDWR)
+            except:
+                pass
+            self.leader_socket.close()"""
             print "Successfully started socket at port " + str(self.leader_socket_port) + "\n"
         except Exception, e: 
-            print "Exception: " + str(e)
+            sys.stderr.write("Exception: " + str(e))
             self.set_up_leader_socket() 
 
     @threaded
     def connect_to_leader_socket(self, connection_timeout, start_time):
         if self.new_election: 
-            print "New election started by someone else \n"
+            print "New election started by someone else. I am " + str(self.id) + "\n"
             return
         elif time.time() < start_time + connection_timeout:
             try:
@@ -94,13 +98,13 @@ class Bulb(Thread):
                 self.follower_socket.connect(('', self.leader_socket_port))
                 print "\n" + str(self.id) + str(self.id) + str(self.id) + " Sucessfully started follower socket for " + str(self.id) + "\n"
             except Exception, e:
-                print "Connecting follower socket exception " + str(e) + "\n"
+                sys.stderr.write("Connecting follower socket exception " + str(e) + "\n")
                 self.connect_to_leader_socket(connection_timeout, start_time)
         else:
             self.new_election = True
             for bulb in self.bulb_list:
                 bulb.new_election = True  
-            print "Let's start a new leader election after timeout: " + str(connection_timeout) + "\n"
+            print "Let's start a new leader election after timeout: " + str(connection_timeout) + ". I am " + str(self.id) + "\n"
             return
 
     def run(self):
