d14633175eff01f1c002240a6dace6af7fd3969b
sbooth@college.harvard.edu <sbooth@college.harvard.edu>
2016-04-28 22:04:02 -0400

    tiny changes

--- ConnectedHearts/connect_to_mfi_test.py
+++ ConnectedHearts/connect_to_mfi_test.py
@@ -3,5 +3,5 @@ import paramiko
 c = paramiko.SSHClient()
 c.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 c.connect('192.168.1.20', username='ubnt', password='ubnt') 
-c.exec_command("echo 1 > /proc/power/relay1")
+c.exec_command("echo 1 > /proc/power/relay1 && echo 1 > /proc/power/relay2")
 print "connected?"
