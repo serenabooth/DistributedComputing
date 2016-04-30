14dca457b0f74539c874ffb715f37cfae4ecf5dd
Serena Booth <sbooth@college.harvard.edu>
2016-04-30 11:19:17 -0400

    heartbeat

--- ConnectedHearts/main.py
+++ ConnectedHearts/main.py
@@ -3,6 +3,25 @@ from control_pi import *
 from multiprocessing.queues import Queue
 from multiprocessing import Array
 import ctypes
+from webcam-pulse.get_pulse import *
+
+""" 
+	Get pulse! 
+"""
+parser = argparse.ArgumentParser(description='Webcam pulse detector.')
+parser.add_argument('--serial', default=None,
+                    help='serial port destination for bpm data')
+parser.add_argument('--baud', default=None,
+                    help='Baud rate for serial transmission')
+parser.add_argument('--udp', default=None,
+                    help='udp address:port destination for bpm data')
+
+args = parser.parse_args()
+App = getPulseApp(args)
+pulse_val = App.main_loop()
+while pulse_val == 0:
+    pulse_val = App.main_loop()
+print "FINISHED with pulse " + str(pulse_val)
 
 bulb_objects_dict = {}
 
@@ -31,7 +50,7 @@ for bulb in bulb_objects_dict.values():
     bulb.register_bulbs(bulb_objects_dict)
     bulb.send_uuid()
 
-pi = Pi(bpm = 70, turned_on_list = power_strip_on_list, hosts = hosts)
+pi = Pi(bpm = pulse_val, turned_on_list = power_strip_on_list, hosts = hosts)
 pi.start()
 
 for bulb in bulb_objects_dict.values():
