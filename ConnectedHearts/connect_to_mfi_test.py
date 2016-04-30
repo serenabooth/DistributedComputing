3769133fbcdc5a849846cf23adac4a51030d4180
sbooth@college.harvard.edu <sbooth@college.harvard.edu>
2016-04-29 23:22:36 -0400

    connected to multiple machines over ssh

--- ConnectedHearts/connect_to_mfi_test.py
+++ ConnectedHearts/connect_to_mfi_test.py
@@ -1,11 +1,27 @@
+import sys, os, string, threading
 import paramiko
 
-c = paramiko.SSHClient()
-c.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-c.connect('192.168.1.20', username='ubnt', password='ubnt') 
-<<<<<<< HEAD
-c.exec_command("echo 1 > /proc/power/relay1 && echo 1 > /proc/power/relay2")
-=======
-c.exec_command("echo 1 > /proc/power/relay1  && echo 1 > /proc/power/relay2")
->>>>>>> 6b78baa3256d57abf2265c7e09cbdfcd843f25cd
-print "connected?"
+outlock = threading.Lock() 
+
+cmd = "echo 0 > /proc/power/relay1"
+
+def workon(host):
+    print "connected to " + host
+    c = paramiko.SSHClient()
+    c.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    c.connect(host, username='ubnt', password='ubnt') 
+    c.exec_command(cmd)
+
+def main(): 
+    hosts = ['192.168.1.20', '192.168.1.21']
+    threads = []
+    for h in hosts: 
+        t = threading.Thread(target=workon, args=(h,))
+        t.start()
+        threads.append(t)
+    for t in threads:
+        t.join()
+
+main()
+
+
