f7bc98c731d7de89bdac79af5c3cad54fa6b41a9
Serena Booth <sbooth@college.harvard.edu>
2016-05-07 18:32:49 -0400

    ground truth generating

--- ConnectedHearts/cv_experimental_testing/histogramTest.py
+++ ConnectedHearts/cv_experimental_testing/histogramTest.py
@@ -1,19 +1,24 @@
-import numpy as numpy
-import cv2
+import csv
+from datetime import datetime
 
-cap = cv2.VideoCapture(0)
+with open('experiment_synch_data.csv', 'w') as csvfile:
+    fieldnames = ['time', 'expected_brightness_value']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
-sub_rects = [[0,50,0,50]]
+    writer.writeheader()
 
-frame_id = 0
-last_dark_frame = 0
+    on = 0
+    state_ct = 0
+    for i in range(0, 120 * 60 * 5):
+        # each frame is 8.33333 milliseconds
+        # assuming heartbeat is 60bpm, the bulb should be on for 1 second and off for 1 secs
+        if on: 
+            writer.writerow({'time': str(i), 'expected_brightness_value': '0'})
+        else:
+            writer.writerow({'time': str(i), 'expected_brightness_value': '255'})
 
+        if state_ct == 120: 
+            on = (on + 1) % 2
+            state_ct = 0
 
-while cap.isOpened(): 
-    _, frame = cap.read()
-
-    cv2.imshow('frame', frame[200:250,200:250])
-    cv2.waitKey(1)
-
-cap.release()
-cv2.destroyAllWindows()
+        state_ct += 1
