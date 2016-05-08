c7cb04531aeb99e03ddea596ef9aa6906d96aa5b
Serena Booth <sbooth@college.harvard.edu>
2016-05-08 19:43:43 -0400

    I must be killing processes

--- ConnectedHearts/control_bulb.py
+++ ConnectedHearts/control_bulb.py
@@ -63,42 +63,42 @@ class BulbControl(Process):
                     print "I am continuing"
                     continue
 
-                # TODO: Fix this bad logic
-                steps_to_above = 13
-                steps_to_below = 13
-                for i in range(1,12):
-                    if (self.above_bulb_id + i) % 12 == leader_id:
-                        steps_to_above = min(steps_to_above, i)
-                    if (self.above_bulb_id - i) % 12 == leader_id:
-                        steps_to_above = min(steps_to_above, i)
+                # # TODO: Fix this bad logic
+                # steps_to_above = 13
+                # steps_to_below = 13
+                # for i in range(1,12):
+                #     if (self.above_bulb_id + i) % 12 == leader_id:
+                #         steps_to_above = min(steps_to_above, i)
+                #     if (self.above_bulb_id - i) % 12 == leader_id:
+                #         steps_to_above = min(steps_to_above, i)
 
-                    if (self.below_bulb_id + i) % 12 == leader_id:
-                        steps_to_below = min(steps_to_below, i)
-                    if (self.below_bulb_id - i) % 12 == leader_id:
-                        steps_to_below = min(steps_to_below, i)
+                #     if (self.below_bulb_id + i) % 12 == leader_id:
+                #         steps_to_below = min(steps_to_below, i)
+                #     if (self.below_bulb_id - i) % 12 == leader_id:
+                #         steps_to_below = min(steps_to_below, i)
 
-                if (steps_to_above < steps_to_below): 
-                    closer_time = self.time_of_neighbor_above
-                else:
-                    closer_time = self.time_of_neighbor_below
+                # if (steps_to_above < steps_to_below): 
+                #     closer_time = self.time_of_neighbor_above
+                # else:
+                #     closer_time = self.time_of_neighbor_below
 
-                if (closer_time == self.comp_above_time or 
-                        closer_time == self.comp_below_time):
-                    time.sleep(0.5)
-                    print "I am continuing"
-                    continue
-                # timedelta
-                time_diff = self.time_of_last_blink - closer_time
-                # convert timedelta to seconds
-                microseconds = time_diff.microseconds
+                # if (closer_time == self.comp_above_time or 
+                #         closer_time == self.comp_below_time):
+                #     time.sleep(0.5)
+                #     print "I am continuing"
+                #     continue
+                # # timedelta
+                # time_diff = self.time_of_last_blink - closer_time
+                # # convert timedelta to seconds
+                # microseconds = time_diff.microseconds
 
-                if microseconds < 0:
-                    self.adjustment.value = -0.05
-                else:
-                    self.adjustment.value = 0.05
-                print "I, " + str(self.id) + " am making an adjustment of " + str(self.adjustment.value)
+                # if microseconds < 0:
+                #     self.adjustment.value = -0.05
+                # else:
+                #     self.adjustment.value = 0.05
+                # print "I, " + str(self.id) + " am making an adjustment of " + str(self.adjustment.value)
                 
-                self.comp_time = self.time_of_last_blink
+                # self.comp_time = self.time_of_last_blink
                 time.sleep(1)
                 # self.comp_above_time = self.time_of_neighbor_above
                 # self.comp_below_time = self.time_of_neighbor_below
