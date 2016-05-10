b3096bb357ed39ec36cbdca7f73609d7eee74093
Michelle Cone <mcone@college.harvard.edu>
2016-05-10 06:00:38 -0400

    Why is this broken now?

--- ConnectedHearts/hearts.py
+++ ConnectedHearts/hearts.py
@@ -58,11 +58,12 @@ class Bulb(Process):
         """
         Initializes a Bulb process
 
-        :param id: The unique id of the bulb which acts as its name, ranges from 0 to 12
+        :param id: The unique id of the bulb which acts as its name, ranges from 
+        0 to 12
         :type id: long
-        :param turned_on_list: Used to keep track of which bulbs are currently turned 
-        on. The list is of length 13 with information about the status of a particular
-        bulb located at the index equal to its id.
+        :param turned_on_list: Used to keep track of which bulbs are currently 
+        turned on. The list is of length 13 with information about the status of 
+        a particular bulb located at the index equal to its id.
         :type turned_on_list: multiprocessing.Array
         :type bpm: The pulse that the bulb should use when determining when to turn
         on and off. This is calculated using the webcame_pulse library. 
@@ -115,13 +116,16 @@ class Bulb(Process):
         self.bpm = bpm
         self.host = host
 
+        if self.id == 0:
+            self.uuid = 2**64-2
+
     def register_bulbs(self, bulb_objects_list):
         """
-        Sets self.bulb_object_list equal to a list of all the bulb process objects in
-        the system.
+        Sets self.bulb_object_list equal to a list of all the bulb process 
+        objects in the system.
 
-        :param bulb_objects_list: A list of all the bulb process objects in the system,
-        which is passed in by the main thread.
+        :param bulb_objects_list: A list of all the bulb process objects in the 
+        system, which is passed in by the main thread.
         :type bulb_objects_list: list of Bulb()s
         :return: None
         """
@@ -189,8 +193,9 @@ class Bulb(Process):
 
     def send_new_election_msg(self):
         """
-        Sets bulbs_in_election to a list of bulb objects with uuids higher than self.uuid. 
-        Then self puts a new election message on the election_q of all of these bulbs.
+        Sets bulbs_in_election to a list of bulb objects with uuids higher than 
+        self.uuid. Then self puts a new election message on the election_q of 
+        all of these bulbs.
 
         :return: None
         """
@@ -213,11 +218,13 @@ class Bulb(Process):
 
     def first_leader_election(self):
         """
-
+        A bulb process finds the max uuid in its election_q and then chooses
+        the leader accordingly.
 
         Has the invariant that all bulbs know the uuid of all other bulbs that are
         running. 
 
+        :return: None
         """
         self.leader = self.uuid_dict[self.get_max_uuid()]
         self.leader_id.value = self.leader.id
@@ -226,32 +233,52 @@ class Bulb(Process):
         if self.leader.id == self.id:
             print ("Hi, I'm the leader: " + str(self.id) + " Right? " + 
                     str(self.leader == self) + "\n")
+            # turn myself on
             self.turn_on()
+            # then respond to pings from followers
             self.respond_to_ping()
         else:
             print "Hi, I'm a follower: " + str(self.id) + "\n"
+            # start a new process to check for when I should turn on
             p = Process(target=self.check_turn_on, args=())
             p.start()
+            # start pinging the leader
             self.election_q.put("first ping")
             self.ping_leader()
 
     def check_turn_on(self):
+        """
+        Continuously checks to see if there is something on the state_q.
+        If there is something on the state_q, that means that a neighbor has
+        told it to turn on so it turns itself on.
+
+        :return: None
+        """
         while (self.state_q.empty()):
             time.sleep(1)
         self.state_q.get()
         self.turn_on()
 
     def turn_on(self):
+        """
+        Starts a new process called bulb_control and then signals to its neighbors
+        that they should turn on.
+
+        bulb_control handles anything related to physically turning the bulb on
+        and off.
+
+        :return: None
+        """
         self.turned_on_list[self.id] = 1
 
-        bulbControl = BulbControl(  my_id = self.id,
+        """bulb_control = BulbControl(  my_id = self.id,
                                     bpm = self.bpm, 
                                     host = self.host,
                                     leader_id = self.leader_id,
                                     state_q = self.state_q,
                                     bulb_objects_list = self.bulb_objects_list, 
                                     turned_on_list = self.turned_on_list)
-        bulbControl.start()
+        bulb_control.start()"""
         neighbor_above_id = (self.id + 1) % 13
         neighbor_below_id = (self.id - 1) % 13 
 
@@ -265,13 +292,25 @@ class Bulb(Process):
         if len(neighbors_to_signal_to) > 0: 
             self.signal_to_neighbors(neighbors_to_signal_to)
 
+    def signal_to_neighbors(self, list_of_neighbors):
+        """ 
+        Wait for ping_time and then tell neighbors to turn on
+
+        :param list_of_neighbors: The bulb neighbors of self
+        :type param: list of Bulb()s
+        :return None:
+        """
+        time.sleep(self.ping_time)
+        for neighbor in list_of_neighbors: 
+            neighbor.state_q.put(1)
+
     def ping_leader(self):
         """
-        Repeatedly ping the leader after waiting ping_time, if you have received a 
-        message from the leader.
+        Repeatedly ping the leader after waiting ping_time, if you have received 
+        a message from the leader.
 
-        If you receive a new election or new leader message, prepare for a new leader
-        election and return.
+        If you receive a new election or new leader message, prepare for a new 
+        leader election and return.
 
         If the leader does not respond after max_timeout, start a new election and
         return.
@@ -300,18 +339,6 @@ class Bulb(Process):
                 return
         self.ping_leader()
 
-    def signal_to_neighbors(self, list_of_neighbors):
-        """ 
-        Wait for ping_time and then tell neighbors to turn on
-
-        :param list_of_neighbors: The bulb neighbors of self
-        :type param: list of Bulb()s
-        :return None:
-        """
-        time.sleep(self.ping_time)
-        for neighbor in list_of_neighbors: 
-            neighbor.state_q.put(1)
-
     def respond_to_ping(self):
         """
         Respond to the pings of all the followers. Continuously get messages from
@@ -325,11 +352,12 @@ class Bulb(Process):
 
         Then continue responding to pings.
 
-        If you receive a new election or new leader message, prepare for a new leader
-        election and return.
+        If you receive a new election or new leader message, prepare for a new 
+        leader election and return.
 
         :return: None
         """
+        print "Responding to pings"
         while not self.election_q.empty():
             pinger_uuid = self.election_q.get()
             if "New election" in str(pinger_uuid) or "New leader" in str(pinger_uuid):
@@ -338,7 +366,9 @@ class Bulb(Process):
                     #str(self.uuid_dict[initiator_uuid].id) + 
                     #" told me there was a new election \n")
                 self.set_bulbs_in_new_election(initiator_uuid)
-                return           
+                return 
+            if self.id == 0:
+                time.sleep(30000)         
             #print "I responded to bulb " + str(self.uuid_dict[pinger_uuid].id) + "\n")
             self.uuid_dict[pinger_uuid].election_q.put(self.uuid)
         time.sleep(self.ping_time)
@@ -349,51 +379,87 @@ class Bulb(Process):
         self.respond_to_ping()
 
     def new_leader_election(self):
+        """
+        A slight modification to the Bully algorithm.
+
+        This is run if a leader ever becomes non-responsive and a process receives
+        a "new election" message on its election_q
+
+        :return: None
+        """
         timeout = time.time() + self.max_timeout
         responses = []
+        # wait for responses from higher uuids for max_timeout 
         while time.time() < timeout:
+            # if the election_q isn't empty, decide what to do with the msg
             if not self.election_q.empty(): 
                 msg = self.election_q.get()
+                # if the msg is a new election, send your uuid to the appropriate
+                # bulbs
                 if "New election" in str(msg):
                     initiator_uuid = long(msg.split(": ")[1])
                     bulbs_in_election = [bulb for bulb in self.bulb_objects_list 
                                             if bulb.uuid >= initiator_uuid and 
                                             bulb.uuid != self.uuid]
                     self.send_uuid(bulbs_in_election)
-                else:
+                # if the msg is a uuid
+                if type(msg) == type(long(1)):
+                    # add it to responses if it is not already in responses
                     if msg not in responses:
                         responses.append(msg)
+
+        # sort responses from lowest to highest uuid
         responses.sort()
+
+        # this means you are the leader
         if not responses or self.uuid > responses[len(responses) - 1]:
             print ("Responses: " + str(responses) + " I'm the LEADER and I'm bulb " 
                 + str(self.id) + "\n")
             print datetime.datetime.now()
             self.leader = self
+            # send a new leader message to all of the bulbs
             for bulb in [bulb for bulb in self.bulb_objects_list if bulb is not self]:
+                print "Sending to bulb " + str(bulb.id) + "\n"
                 bulb.election_q.put("New leader: " + str(self.uuid))
+
+            # empty your election_q before responding to pings
             self.empty_q(self.election_q)
             self.respond_to_ping()
+
+        # this means you are not the leader
         else:
             print "I'm not the leader and I'm bulb " + str(self.id) + "\n"
             new_leader = False
             timeout = time.time() + 2 * self.max_timeout
+            # wait for a new leader message
             while time.time < timeout:
                 if not self.election_q.empty():
                     msg = self.election_q.get()
+                    # if you receive a new leader message
                     if "New leader" in str(msg):
+                        print "Got a new leader message"
                         leader_uuid = long(msg.split(": ")[1])
                         if self.uuid < leader_uuid:
+                            # and your uuid is lower than the new leaders uuid
+                            # then set leader to the new leader
                             self.leader = self.uuid_dict[leader_uuid]
                             new_leader = True
                             break
+            # empty your election_q
             self.empty_q(self.election_q)
+
             if new_leader:
+                "New leader is true"
+                # if there is a new leader, wait max_timeout for it to start
+                # responding to pings
                 time.sleep(self.max_timeout)
                 print ("I'm bulb " + str(self.id) + " and I think the leader is " 
                     + str(self.leader.id) + "\n")
+                # then start pinging the new leader
                 self.election_q.put("first ping")
                 self.ping_leader()
             else:
+                # if you never received a new leader message, start another election
                 self.send_new_election_msg()
                 return
 
